271246a810268c64a435e9c4215f653b4651ec3b
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2023-12-30 02:15:33 +0100

    add-js

---
+++ js.js
@@ -0,0 +1,51 @@
+// const newTag = document.createElement('h2');
+// newTag.classList.add('newTagClass');
+// newTag.textContent = 'Best employee of the year';
+// newTag.style.textAlign = 'center';
+// newTag.style.marginBottom = '20px';
+// console.log(newTag);
+// const image = document.createElement('img');
+// image.src =
+//   'https://www.wfla.com/wp-content/uploads/sites/71/2023/05/GettyImages-1389862392.jpg?w=2560&h=1440&crop=1';
+// image.alt = 'Nature';
+// image.style.border = '5px solid red';
+// image.width = '500';
+// image.style.margin = '0 auto';
+// image.style.marginBottom = '20px';
+// console.log(image);
+// const elem = document.querySelector('.our-team-title');
+// console.log(elem);
+// elem.after(newTag, image);
+// document.querySelector('.our-team-title').remove();
+
+// const newDiv = document.createElement('div');
+// const title = document.createElement('h2');
+// const text = document.createElement('p');
+// title.textContent = 'This is title';
+// text.textContent = 'This is text';
+// newDiv.append(title, text);
+// console.log(newDiv);
+// const test = document.querySelector('.our-team .container');
+// console.log(test);
+// test.innerHTML = '';
+// test.remove();
+// const button = document.querySelector('.main-button');
+
+// button.addEventListener('click', () => {
+//   console.log('The button was pressed and now the next image will appear');
+// });
+// const elem = document.querySelectorAll('.employee');
+// console.log(elem);
+
+// const newElem = [...elem];
+// console.log(newElem.length);
+// newElem.map((item) => {
+//   const ourTeamContent = item
+//     .querySelector('.our-team-content')
+//     .textContent.toLowerCase();
+//   ourTeamContent.includes('designer')
+//     ? (item.style.backgroundColor = 'green')
+//     : ourTeamContent.includes('marketing')
+//     ? (item.style.backgroundColor = 'red')
+//     : (item.style.backgroundColor = 'blue');
+// });
